99ec6739e9bc13c665507221406facbe988245a7
thachanhtuan <thachanhtuan@proton.me>
2025-12-23 18:52:57 +0500

    PR2: Структура каталогов. базовые функции в modal.js, заглушка в project-page для получения данных

--- src/app/pages/projects-page.js
+++ src/app/pages/projects-page.js
@@ -1,3 +1,7 @@
+import {getAllProjects, getProjectById, getCurrentProject, appDataInit, createProject, updateProject, deleteProject,} from "../../services/project-service.js";
+import { openModal, closeModal} from "../../ui/modal.js";
 
+// ============================================================
+document.addEventListener("DOMContentLoaded", async () => { })
 
 console.log("Projects Page Module Loaded");
--- src/ui/modal.js
+++ src/ui/modal.js
@@ -1,1 +1,13 @@
- 
+function openModal(modalId) {
+  const modal = document.getElementById(modalId);
+  if (!modal) return;
+  modal.classList.add("active");
+}
+
+function closeModal(modalId) {
+  const modal = document.getElementById(modalId);
+  if (!modal) return;
+  modal.classList.remove("active");
+}
+
+export { openModal, closeModal };
